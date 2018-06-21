89627e818be8d0dbe62930a5d203b9cc651f8642
yaelatletl <yael.atletl@gmail.com>
2018-06-21 14:55:35 -0500

    mainmenu added

--- addons/LensFlare/lens-flare-plugin.gd
+++ addons/LensFlare/lens-flare-plugin.gd
@@ -2,10 +2,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type(
-		"LensFlare", "Node",
-		preload("res://addons/LensFlare/lens-flare.gd"),
-		preload("res://addons/LensFlare/lens_flare_icon.png")
+	add_custom_type("LensFlare", "Node",preload("res://addons/LensFlare/lens-flare.gd"),preload("res://addons/LensFlare/lens_flare_icon.png")
 	)
 
 func _exit_tree():
--- addons/WorldManagement/World.gd
+++ addons/WorldManagement/World.gd
@@ -7,6 +7,6 @@ extends EditorPlugin
 func _enter_tree():
 	add_custom_type("World", "Navigation", preload("WorldManagement.gd"), preload("icon.png"))
 	add_custom_type("Gravity Area", "Area", preload("GravityArea.gd"), preload("Gravicon.png"))
-	add_custom_type("AI","Node",preload("BASIC_AI.gd"), preload("IA_icon.png"))
+	#add_custom_type("AI","Node",preload("BASIC_AI.gd"), preload("IA_icon.png"))
 func _exit_tree():
 	remove_custom_type("World")
