6ea05a25e55f6531ba9e068f324023e7b7fca892
yaelatletl <yael.atletl@gmail.com>
2018-06-25 06:50:02 -0500

    Moving stuff, adding fonts, adding global configuration
    Now, this is kind of a big commit, not in code terms but in bytes. So... things that changed:
    +Anonymus Pro as sustitute of the Marathon Terminal's font.
    +A green rectangle as placeholder
    +Moved the main menu to somewhere else
    +Added the Marathon Logo so we can use it anywhere we need.
    +Started to script the Settings of the game, visuals first.

---
+++ BaseGD/Scenes/menu/menu.gd
@@ -0,0 +1,31 @@
+extends Viewport
+
+var counter = 0
+var actual_menu = 0
+
+func _ready():
+	
+	
+	var MSAA = ProjectSettings.get_setting("filters/msaa")
+	var SSAO = ProjectSettings.get_setting("env/s_s_ambient_occlusion")
+	var SSR = ProjectSettings.get_setting("env/s_s_reflections")
+	var ANSIO = ProjectSettings.get_setting("filters/ansiotropic_filter_levels")
+	
+	
+func _process(delta):
+	if actual_menu == 0:
+		if Input.is_action_pressed("ui_up"):
+			counter +=1
+		if Input.is_action_pressed("ui_down"):
+			counter -=1
+		if counter > 3:
+			counter = 0
+		if counter < 0: 
+			counter = 3
+		
+func _Accept():
+	if actual_menu == 0 and counter == 0:
+		start_game()
+		
+func _done():
+	ProjectSettings.save()
