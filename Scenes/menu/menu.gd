2dd31f99bc6192f2f6bdbdbd1fc79a6086bce2aa
yaelatletl <yael.atletl@gmail.com>
2018-11-04 00:16:41 -0600

    Fixed Menu, Added Terminal theme

--- Scenes/menu/menu.gd
+++ Scenes/menu/menu.gd
@@ -12,29 +12,9 @@ func _ready():
 	var ANSIO = ProjectSettings.get_setting("filters/ansiotropic_filter_levels")
 	
 	
-func _process(delta):
-	if Input.is_action_pressed("ui_up"):
-			counter +=1
-	if Input.is_action_pressed("ui_down"):
-			counter -=1
-	if Input.is_action_pressed("ui_accept"):
-		_Accept()
-		
-	if actual_menu == 0:
-		if counter > 3:
-			counter = 0
-		if counter < 0: 
-			counter = 3
+
 			
-	if actual_menu == 1:
-		if counter > 3:
-			counter = 0
-			
-func _Accept():
-	if actual_menu == 0 and counter == 0:
-		start_game()
-	if actual_menu == 0 and counter == 1: 
-		hehehe()
+
 		
 func _done():
 	ProjectSettings.save()
