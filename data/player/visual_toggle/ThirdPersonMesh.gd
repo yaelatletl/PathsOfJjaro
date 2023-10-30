b32825f6fe7f1cd1919d8bfe030fb509b83cd45b
yaelatletl <yael.atletl@gmail.com>
2023-10-30 12:32:22 -0600

    remove duplicate files

--- data/player/visual_toggle/ThirdPersonMesh.gd
+++
@@ -1,9 +0,0 @@
-extends MeshInstance3D
-
-func _ready() -> void:
-	var mpAPI = get_tree().get_multiplayer()
-	if mpAPI.has_multiplayer_peer():
-		if mpAPI.is_server():
-			set_layer_mask(4)
-	else:
-		set_layer_mask(4)
