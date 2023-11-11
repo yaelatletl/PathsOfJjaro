315b7ac85eca26db351a3072484023041dbf1bce
yaelatletl <yael.atletl@gmail.com>
2023-11-11 10:15:18 -0600

    Fix crouching again

--- data/actors/shared/m1_movement.gd
+++ data/actors/shared/m1_movement.gd
@@ -23,6 +23,11 @@ var elapsed_coyote_time : float = 0
 @onready var col = get_node(collision)
 @onready var feet = get_node(feet_path)
 
+#We're gonna hardcode some names here
+@onready var minimal_collision = get_node("../minimal")
+@onready var separation_ray = get_node("../separation_ray")
+
+
 var _delta
 var impulse = Vector3.ZERO
 
@@ -41,6 +46,9 @@ func _functional_routine(input : Dictionary) -> void:
 		_crouch(input, _delta)
 		# Function for sprint
 		_sprint(input, _delta)
+#Changed it a bit respecting to our regular movement, this one requires very specific colliders
+#Otherwise the behaviour will be off and it wont be able to do platform-stair climbing
+#Crouching most probably will be disabled, but implementation stays just in case
 
 func _movement(input : Dictionary, _delta : float) -> void:
 	actor.direction = Vector3()
@@ -79,7 +87,7 @@ func _movement(input : Dictionary, _delta : float) -> void:
 	#Accidentally implemented wall climbing....
 	#var test_transform = Transform3D(actor.transform.basis, actor.transform.origin + Vector3(0, step_treshold, 0) + (actor.linear_velocity)* _delta) #Treshold set at 1.2
 	#var should_go_up = actor.test_move(actor.transform, actor.linear_velocity * _delta) and not actor.test_move(test_transform, actor.linear_velocity * _delta)
-    
+	
 	#Stairs/ledge step check
 	#Do a test move, check if the character could go up a step or down a step
 	#check if the character would be stopped if going forward, then if it would be stopped if going forward and up. Then it should go up. 
@@ -95,7 +103,7 @@ func _movement(input : Dictionary, _delta : float) -> void:
 	actor.set_floor_stop_on_slope_enabled(false)
 	actor.set_max_slides(4)
 	actor.set_floor_max_angle(PI/4)
-    # using the step_treshold variable
+	# using the step_treshold variable
 	# TODOConverter40 infinite_inertia were removed in Godot 4.0 - previous value `false`
 	actor.move_and_slide()
 	actor.linear_velocity = actor.velocity
@@ -109,7 +117,7 @@ func _movement(input : Dictionary, _delta : float) -> void:
 	
 func _crouch(input : Dictionary, _delta :float) -> void:
 	# Inputs
-	if not col:
+	if not col or not separation_ray:
 		return
 	# Get the character's head node
 	# If the head node is not touching the ceiling
@@ -120,11 +128,12 @@ func _crouch(input : Dictionary, _delta :float) -> void:
 		var shape = col.shape.height
 		
 		# Changes the shape of the character's collision
-		shape = lerp(shape, 1.7 - (get_key(input, "crouch") * 1.2), w_speed  * _delta)
+		shape = lerp(shape, 1.7 - (get_key(input, "crouch") * 1.5), w_speed  * _delta)
 		
 		# Apply the new character collision shape
 		col.shape.height = shape
 		col.shape.radius = (0.24 - 0.12*get_key(input, "crouch"))
+		separation_ray.shape.length = shape
 		feet.target_position.y = -shape
 
 func _sprint(input : Dictionary, _delta : float) -> void:
@@ -134,20 +143,16 @@ func _sprint(input : Dictionary, _delta : float) -> void:
 		# switch between sprint and walking
 		actor.reset_slide_multi()
 		var toggle_speed : float = w_speed + ((s_speed - w_speed) * get_key(input, "sprint")) #Normal Sprint Speed
-		
-		if actor.is_on_wall() and actor.is_far_from_floor() and actor.run_speed>12 and can_wallrun:
-			toggle_speed *= actor.wall_multiplier #On wall sprint speed
-			actor.wall_multiplier = lerp(actor.wall_multiplier, 1.0, _delta/20)
 		# Create a character speed interpolation
 		n_speed = lerp(n_speed, toggle_speed, 3 * _delta)
 	else:
 		# Create a character speed interpolation
 		actor.linear_velocity.y -= 0.1*actor.run_speed*_delta
-		if slide_on_crouch and actor.run_speed>12 and actor.is_on_floor():
+		if slide_on_crouch and actor.run_speed>12 and not actor.is_far_from_floor():
 			n_speed = lerp(n_speed, w_speed * actor.multiplier , 15* _delta)
 			actor.multiplier = lerp(actor.multiplier, 0.8, _delta*20)
 			return
-		elif actor.is_on_floor():
+		elif not actor.is_far_from_floor():
 			n_speed = lerp(n_speed, c_speed, actor.multiplier*_delta)
 			actor.reset_slide_multi()
 			actor.reset_wall_multi()
